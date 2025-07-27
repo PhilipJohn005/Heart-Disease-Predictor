d163c04f33fdefc7427796651de4d7856fc519d4
PhilipJohn005 <pjdeluge123@gmail.com>
2025-07-27 13:20:48 +0530

    Refactor:Modified Hyperparametrs for logistic regression

--- model.py
+++ model.py
@@ -25,7 +25,7 @@ def train_model():
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
     print("Training Model...")
-    model = LogisticRegression(max_iter=1000)
+    model = LogisticRegression(max_iter=2000)
     model.fit(X_train, y_train)
 
     y_pred = model.predict(X_test)
